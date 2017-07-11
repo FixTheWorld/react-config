a59a96c908f7dddfa235e6e50ef6c660ebb554b3
forceking3 <2561037140@qq.com>
2017-07-11 16:53:17 +0800

    生产与开发环境中chunk名称以及css热加载

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,16 +4,17 @@
 const webpack=require('webpack');
 const path=require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");//提取css，不支持热更新
+const SpriteSmithPlugin = require('webpack-spritesmith');
 const prefix=require('autoprefixer');
-const SpritesmithPlugin = require('webpack-spritesmith');
-// const px2rem = require('postcss-px2rem');
-// const CompressionWebpackPlugin = require('compression-webpack-plugin');//gzip压缩，需要服务器开启gzip转换
 const config=require('./config');
 const env=config.env;
-const webpackConfig= {
+const entry=path.resolve('', "./index.js");
+const publicPath="/world-encyclopedia/";
+
+let webpackConfig= {
     entry: {
-        "js/app": path.resolve('', "./index.js"),
+        "js/app":entry,
         "js/vendor":[
             'react',
             'react-redux',
@@ -22,10 +23,10 @@ const webpackConfig= {
         ]
     },
     output: {
-        path:path.resolve('',"./build"),
-        filename: "[name].[hash].js",
-        // publicPath:'',//默认是当前路径，可以写入“/”来做成绝对路径
-        chunkFilename:"js/[name].[hash].js"//按需加载的js文件的路径和后缀名
+        filename:config.env==='development'?"[name].[hash].js":"[name].[chunkhash].js",
+        path: path.resolve('', './docs'),
+        publicPath:publicPath,
+        chunkFilename:"js/[name].[chunkhash].js"//按需加载的js文件的路径和后缀名
     },
     module:{
         rules:
@@ -41,7 +42,8 @@ const webpackConfig= {
                 use:ExtractTextPlugin.extract({fallback:"style-loader", use:"css-loader"})
             },{
                 test:/\.scss$/,
-                use:ExtractTextPlugin.extract({
+                use:config.env==='development'?["style-loader","css-loader","sass-loader"]:
+                ExtractTextPlugin.extract({
                     fallback:"style-loader",
                     use:["css-loader",{
                         loader:"postcss-loader",
@@ -64,61 +66,33 @@ const webpackConfig= {
         ]
     },
     devServer: {
-        port:2355,
+        port:612,
         hot:true,
-        historyApiFallback: true,//不跳转
+        historyApiFallback:{index:publicPath},//跳转到指定路径
         inline: true//实时刷新
     }
 };
 webpackConfig.plugins=[
-    //定义全局变量
+    //定义全局变量,主要用于webpack对生产和开发环境的判断
     new webpack.DefinePlugin({
         'process.env.NODE_ENV':JSON.stringify(config.env)
     }),
-    //提取html并将相应打包后的js注入到body底部
-    new HtmlWebpackPlugin({
-        template :path.resolve('',"./index.html"),
-        hash     : false,
-        filename : 'index.html',
-        inject   : 'body',
-
-    }),
-    //将指定路径下的图片合并为雪碧图
-    new SpritesmithPlugin({
-        src: {
-            cwd: path.resolve('', 'src/assets/img'),
-            glob: '*.png'
-        },
-        //图片和样式的输出目录
-        target: {
-            image: path.resolve('', 'src/assets/img/sprite/sprite.png'),
-            css: path.resolve('', 'src/assets/css/sprite.css')
-        },
-        apiOptions: {
-            cssImageRef: '../img/sprite/sprite.png'//css文件相对sprite文件的路径
-        },
-        spritesmithOptions: {
-            algorithm: 'top-down'
-        }
-    }),
     new ExtractTextPlugin({filename:"styles.css",allChunks:true}),
     //提取通用js文件，缺省chunks参数将会把入口entry中的通用文件提取到vendor中
+    new HtmlWebpackPlugin({
+        template :path.resolve('',"index.html"),
+        hash     : false,
+        filename :"index.html",
+        inject   : 'body',
+        favicon  :path.resolve('',"src/assets/img/favicon.ico")
+    }),
     new webpack.optimize.CommonsChunkPlugin({
         names : ['js/vendor']
-    }),
-    // new CompressionWebpackPlugin({ //gzip 压缩,需要服务器开启gzip转换
-    //     asset: '[path].gz[query]',
-    //     algorithm: 'gzip',
-    //     test: new RegExp(
-    //         '\\.(js|css)$'    //压缩 js 与 css
-    //     ),
-    //     threshold: 10240,
-    //     minRatio: 0.8
-    // })
+    })
 ];
 //按环境加载插件
 if(env==='development') {
-    webpackConfig.devtool="cheap-module-eval-source-map";
+    webpackConfig.devtool="source-map";
     webpackConfig.plugins.push(
         new webpack.HotModuleReplacementPlugin(),//hmr热替换
         new webpack.NoEmitOnErrorsPlugin());//出错不影响执行
@@ -132,7 +106,7 @@ if(env==='development') {
                 warnings  : false
             }
         }),
-        new webpack.optimize.AggressiveMergingPlugin(),
+        new webpack.optimize.AggressiveMergingPlugin(),//平均分配chunk的大小
         new webpack.optimize.OccurrenceOrderPlugin()//按发生次数分配模块和块id，减少总文件大小
     );
 }
